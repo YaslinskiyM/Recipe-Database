f4aaa98a4c0a6faa9282549468092f39be31ffc8
Tony Yu <ttonyyu98@gmail.com>
2023-05-09 20:36:24 -0700

    sequalize table

---
+++ model/recipe.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connections');
+
+class Recipe extends Model {}
+
+Recipe.init(
+    /*
+    CREATE TABLE `recipe` (
+  `id` int NOT NULL,
+  `recipe_name` varchar(45) NOT NULL,
+  `recipe_description` varchar(45) DEFAULT NULL,
+  `recipe_steps` varchar(255) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `keywords` varchar(45) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`id`,`user_id`,`category_id`),
+  KEY `fk_recipe_user_idx` (`user_id`),
+  KEY `fk_recipe_category1_idx` (`category_id`),
+  CONSTRAINT `fk_recipe_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `fk_recipe_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+    */
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        recipe_name: {
+            
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        recipe_description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        recipe_steps: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        keywords: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+      }
+)
+
+module.exports = Recipe;
---
+++ model/user.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connections');
+
+class User extends Model {}
+
+User.init(
+  {
+    // define columns
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    login_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
+
+
+/*
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `login_id` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `favorite_id` int NOT NULL,
+  `saved_id` int NOT NULL,
+  PRIMARY KEY (`id`,`favorite_id`,`saved_id`),
+  KEY `fk_user_favorite1_idx` (`favorite_id`),
+  KEY `fk_user_saved1_idx` (`saved_id`),
+  CONSTRAINT `fk_user_favorite1` FOREIGN KEY (`favorite_id`) REFERENCES `favorite` (`id`),
+  CONSTRAINT `fk_user_saved1` FOREIGN KEY (`saved_id`) REFERENCES `saved` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+*/
