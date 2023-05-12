ee8ace712c9807c2b73f78bd8ebf4acb441838c0
Tony Yu <ttonyyu98@gmail.com>
2023-05-11 20:01:39 -0700

    server.js/package.js error, module export for recipe route added

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connections');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers/'));
+app.use(require('./routes/'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
