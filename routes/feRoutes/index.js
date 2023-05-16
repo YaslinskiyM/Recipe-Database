0158d4519c1517775bc147629680db5d9481689a
radhikabandi <radhika.leo@gmail.com>
2023-05-15 20:52:20 -0700

    .

--- routes/feRoutes/index.js
+++ routes/feRoutes/index.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 
-router.get('/users', async (req, res) => {
-    console.log('cming here')
+router.get('/', async (req, res) => {
+    console.log('cming here0')
     res.render('users', {
+      });
+
+
+});
+
+
+router.get('/home', async (req, res) => {
+    console.log('cming here0')
+    res.render('homepage', {
+      });
+
+
+});
+
+
+router.get('/signUp', async (req, res) => {
+    console.log('cming here0')
+    res.render('login', {
       });
 
 
--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connections');
+const hbs = exphbs.create({
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 app.use(require('./routes'));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () =>  console.log(`App listening at http://localhost:${PORT} 🚀`));
   });
-  
