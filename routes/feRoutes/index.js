42f233a861012b5fc867895b3f83215170cb4491
Tony Yu <ttonyyu98@gmail.com>
2023-05-17 11:27:20 -0700

    /users/home route

--- routes/feRoutes/index.js
+++ routes/feRoutes/index.js
@@ -8,21 +8,36 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/users/:firstName/home', async (req, res) => {
-  try {
-    // Retrieve the user ID from the URL parameters
-    const firstName = req.params.firstName;
-    //const recipe = req.query.recipe;
-   // console.log('recipes in Route',req.query.recipe)
-    // Render the user-specific home page and pass the user data to the template
-    res.render('homepage', { firstName});
+router.get("/users/home", async (req, res) => {
+	try {
+		/*
+    using req.session.id to grab the user id from the session
+    then using that id to find the user in the database
+    */
+   console.log('req.session.id',req.session.value)
+		const data = await User.findByPk(req.session.value, {
+			attributes: { exclude: ["password"] },
 
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-})
+		});
+    //find all recipes create by user
+    const recipeData = await Recipe.findAll({
+      where: {
+        user_id: req.session.value
+      }
+    })
+    console.log(data)
+    console.log('recipeData',recipeData[0])
+		// Retrieve the user ID from the URL parameters
 
+		//const recipe = req.query.recipe;
+		// console.log('recipes in Route',req.query.recipe)
+		// Render the user-specific home page and pass the user data to the template
+		res.render("homepage", { data , recipeData});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+});
 router.get('/login', async (req, res) => {
      res.render('login');
 });
