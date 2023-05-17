5ca4cb84c27519acda23ed288ba28a75d36617e7
Tony Yu <ttonyyu98@gmail.com>
2023-05-16 21:12:42 -0700

    login ininininini

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/connections');
 const hbs = exphbs.create({
 });
 
-const hbs = exphbs.create();
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+  id : 0,
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,11 +40,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 //process the cookie
-app.use(session({
-  secret: 'YourSecretKey',
-  resave: false,
-  saveUninitialized: false
-}));
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () =>  console.log(`App listening at http://localhost:${PORT} 🚀`));
