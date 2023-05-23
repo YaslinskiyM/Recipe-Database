77bdb5259925e0a24c75acd0eb34a27b3b39fc8d
Tony Yu <ttonyyu98@gmail.com>
2023-05-22 17:19:39 -0700

    testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/connection');
